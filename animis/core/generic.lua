023f4e7c4d326596155402f87f48a141b3f01d08
Lonaasan <shadowlona@shadowlona.dev>
2025-07-06 16:17:33 +0200

    fix: setting directives only when needed

--- animis/core/generic.lua
+++ animis/core/generic.lua
@@ -41,8 +41,10 @@ function init()
     for _, layer in pairs(data) do
         layer.state = ""
         layer.previousState = ""
+        layer.overwrittenState = ""
         layer.oneTime = false
-        layer.timer = 0
+        layer.time = 0
+        layer.previousTime = 0
         layer.maxFrames = layer.maxFrames or config.MAX_FRAMES
         layer.speed = layer.speed or config.ANIMATION_SPEED
         layer.maxRandomValue = layer.maxRandomValue or config.MAX_RAND_VALUE
@@ -97,8 +99,8 @@ function update(dt)
                 newState = layer.state -- continue
             end
 
-            if layer.state == "random" and layer.previousState == newState then
-                if math.floor(layer.timer) < #layer[layer.state] then
+            if layer.state == "random" and layer.overwrittenState == newState then
+                if math.floor(layer.time) < #layer[layer.state] then
                     newState = layer.state
                 end
             end
@@ -133,7 +135,7 @@ function update(dt)
 
             if layer.state ~= newState then
                 idleNum = tonumber(player.personality().idle:match("idle.(%d+)"))
-                layer.timer = 1
+                layer.time = 1
                 layer.oneTime = false
                 layer.state = newState
 
@@ -142,51 +144,56 @@ function update(dt)
             if layer.oneTime == false then
 
                 if layer[layer.state] then
-                    layer.timer = math.min(layer.maxFrames, layer.timer + dt * layer.speed)
+                    layer.time = math.min(layer.maxFrames, layer.time + dt * layer.speed)
 
                     if layer.state == "crouch" and layer.oneTime == false or layer.state == "swimIdle" and layer.oneTime ==
                         false or layer.state == "lounge" and layer.oneTime == false or layer.state:sub(1, 4) == "once" then
                         if layer.state == "crouch" and not layer.crouchIdleLoop or layer.state == "swimIdle" and
                             not layer.swimIdleLoop or layer.state == "lounge" and not layer.loungeIdleLoop then
                             layer.oneTime = true
-                            layer.timer = 1
+                            layer.time = 1
                         else
-                            if math.floor(layer.timer) > #layer[layer.state] then
-                                layer.timer = 1
+                            if math.floor(layer.time) > #layer[layer.state] then
+                                layer.time = 1
                             end
                         end
                     elseif layer.state == "idle" and layer.oneTime == false then
                         if not layer.idleLoop then
                             layer.oneTime = true
-                            layer.timer = idleNum
+                            layer.time = idleNum
                         else
-                            if math.floor(layer.timer) > #layer[layer.state] then
-                                layer.timer = 1
+                            if math.floor(layer.time) > #layer[layer.state] then
+                                layer.time = 1
                             end
                         end
                     elseif layer.state == "jump" or layer.state == "fall" or layer.state:sub(1, 8) == "looponce" or
                         layer.state:sub(1, 6) == "switch" then
-                        if math.floor(layer.timer) > #layer[layer.state] then
-                            layer.timer = #layer[layer.state]
+                        if math.floor(layer.time) > #layer[layer.state] then
+                            layer.time = #layer[layer.state]
                         end
                     elseif layer.state == "walk" or layer.state == "run" or layer.state == "swim" or
                         layer.state:sub(1, 4) == "loop" then
-                        if math.floor(layer.timer) > #layer[layer.state] then
-                            layer.timer = 1
+                        if math.floor(layer.time) > #layer[layer.state] then
+                            layer.time = 1
                         end
                     end
 
-                    -- Apply animation directive
-                    directiveFuncs[layerName](layer[layer.state][math.floor(layer.timer)])
+                    -- Apply animation directive only when frame changes or the state changed
+                    local roundedTime = math.floor(layer.time)
+                    if roundedTime ~= layer.previousTime or layer.state ~= layer.previousState then
+                        directiveFuncs[layerName](layer[layer.state][roundedTime])
+                        layer.previousState = layer.state
+                        layer.previousTime = roundedTime
+                    end
                 end
             end
 
             if math.random(1, layer.maxRandomValue) <= layer.maxRandomTrigger and layer.random and layer.state ~=
                 "random" and layer.state:sub(1, 4) ~= "loop" and layer.state:sub(1, 4) ~= "once" and
                 layer.state:sub(1, 6) ~= "switch" then
-                layer.previousState = layer.state
+                layer.overwrittenState = layer.state
                 layer.state = "random"
-                layer.timer = 1
+                layer.time = 1
                 layer.oneTime = false
             end
         end
